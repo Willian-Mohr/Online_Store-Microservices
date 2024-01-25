bfccdc9b19d26500b165c8eae44df172846d1045
Willian Mohr <willianbmohr@gmail.com>
2024-01-25 18:34:24 -0300

    OSM-0001 Adaptando properties para olhar para o data base docker-container

--- notification-service/src/main/java/com/wohr/notificationservice/NotificationServiceApplication.java
+++ notification-service/src/main/java/com/wohr/notificationservice/NotificationServiceApplication.java
@@ -1,6 +1,7 @@
 package com.wohr.notificationservice;
 
 import lombok.extern.slf4j.Slf4j;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.kafka.annotation.KafkaListener;
@@ -13,11 +14,11 @@ public class NotificationServiceApplication {
         SpringApplication.run(NotificationServiceApplication.class, args);
     }
 
-    @KafkaListener(topics = "notificationTopic")
-    public void handleNotification(OrderPlacedEvent orderPlacedEvent) {
+    @KafkaListener(topics = "notificationTopic", groupId = "notificationId")
+    public void handleNotification(ConsumerRecord<String, OrderPlacedEvent> record) {
+        OrderPlacedEvent orderPlacedEvent = record.value();
         log.info("=========== Consumer notificationTopic ======================= ");
         log.info("Received Notification for Order - {}", orderPlacedEvent.getOrderNumber());
         log.info("============================================================== ");
     }
-
 }
